ad87dd84687b6625be07f02a228c74bde5dfc2db
JUAN JOSE CARRIZO <jjcarrizo@ucasal.edu.ar>
2024-03-22 13:32:14 -0300

    Siguiente commit

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 
-	"github.com/jjcarrizo/godesde0/variables"
+	"github.com/jjcarrizo/godesde0/ejercicios"
+	//"github.com/jjcarrizo/godesde0/variables"
 )
 
 func main() {
-	estado, texto := fmt.Println(variables.ConvertiraTexto(123))
+	/*estado, texto := fmt.Println(variables.ConvertiraTexto(123))
 	fmt.Println(estado)
+	fmt.Println(texto)*/
+	numero, texto := ejercicios.StringToInt("200")
+	fmt.Println(numero)
 	fmt.Println(texto)
 }
