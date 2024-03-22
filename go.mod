e68b236350acab5f0b9e106cc6a6211f5001f9d5
JUAN JOSE CARRIZO <jjcarrizo@ucasal.edu.ar>
2024-03-22 11:34:13 -0300

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jjcarrizo/godesde0
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jjcarrizo/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
